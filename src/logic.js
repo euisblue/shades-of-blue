d2f2119f9e0f05a7cd146c5c676d9805e67ae62a
Blue Eu <euisblue@pm.me>
2021-12-06 23:04:40 +0900

    shades of blue finished

--- src/logic.js
+++ src/logic.js
@@ -2,23 +2,55 @@
     const container = document.querySelector('div.container');
     const containerWrapper = document.querySelector('div.container > .wrapper');
 
-    let requestURL = 'https://raw.githubusercontent.com/yuueu/shades-of-blue/main/src/blue.json';
-    let request = new XMLHttpRequest();
-    request.open('GET', requestURL);
-    request.responseType = 'json';
-    request.send();
-    request.onload = function () {
-        var blue = request.response;
-        console.log(blue);
-        // populateBlue(blue);
+    let req = new XMLHttpRequest();
+    var blue;
+
+    req.onreadystatechange = () => {
+      if (req.readyState == XMLHttpRequest.DONE) {
+          blue = JSON.parse(req.responseText);
+          populate(blue);
+      }
+    };
+    
+    req.open("GET", "https://api.jsonbin.io/b/61ae044701558c731ccf35d8/latest");
+    req.setRequestHeader("secret-key", "$2b$10$qWldbeLTOtl7KSOhqohB/OhZ3SeyKG4H4khqWAgaerXGKB7TqJIny");
+    req.send();
+
+    function populate(blue) {
+        colorNames = Object.keys(blue);
+        for(let i = 0; i<colorNames.length; ++i) {
+            let div = document.createElement('div');
+            let p = document.createElement('p');
+            let p2 = document.createElement('p');
+            let hex = blue[colorNames[i]];
+
+            
+            p2.textContent = hex;
+            p.textContent = colorNames[i];
+
+            div.classList.add("colorpad");
+            div.style.backgroundColor = hex;
+
+            div.append(p);
+            div.append(p2);
+            containerWrapper.append(div);
+
+            div.onclick = () => {
+                copyColor(hex);
+                document.querySelector('div.alert').style.right = "-5px";
+                setTimeout(() => {
+                    document.querySelector('div.alert').style.right = "-205px";
+                },1000);
+            }
+        }
     }
 
-    function populateBlue(blue) {
-        // blue.forEach(element => {
-            // let div = document.createElement('div');
-            // div.textContent = element["name"];
-            // container.append(div);
-        // });
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
 
+    function copyColor(color) {
+        console.log(color);
+        navigator.clipboard.writeText(color);
     }
 })(window);
